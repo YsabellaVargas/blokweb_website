4def8dfdf506f2fed8a6a4df1a7410c3f9b6abd3
YsabellaVargas <70380047+YsabellaVargas@users.noreply.github.com>
2020-10-08 11:11:15 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,23 @@ var hamburgerClose = document.querySelector('nav ul:first-of-type li:last-of-typ
 
 console.log(hamburgerClose)
 
-
 function toonHamburger() {
     document.body.classList.toggle('toonHamburger')
 }
 
 hamburger.addEventListener('click', toonHamburger);
 hamburgerClose.addEventListener('click', toonHamburger)
+
+var quickViewButton = document.querySelector('#quickViewButton');
+var productArticle = document.querySelector('#productArticle');
+
+function showQuickViewButton() {
+    quickViewButton.classList.add('show');
+}
+
+function hideQuickViewButton() {
+    quickViewButton.classList.remove('show');
+}
+
+productArticle.addEventListener('mouseover', showQuickViewButton);
+productArticle.addEventListener('mouseout', hideQuickViewButton);
