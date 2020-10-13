b0e8b0bfa93b6dee66094c5d8d07fdba8e310da2
YsabellaVargas <70380047+YsabellaVargas@users.noreply.github.com>
2020-10-13 17:14:56 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // JavaScript Document
-// JavaScript Document
 
+//hamburgermenu
 var hamburger = document.querySelector('nav ul:nth-of-type(2) li button');
 console.log(hamburger)
 var hamburgerClose = document.querySelector('nav ul:first-of-type li:last-of-type button');
@@ -8,26 +8,26 @@ var hamburgerClose = document.querySelector('nav ul:first-of-type li:last-of-typ
 console.log(hamburgerClose)
 
 function toonHamburger() {
-    document.body.classList.toggle('toonHamburger')
+  document.body.classList.toggle('toonHamburger')
 }
 
 hamburger.addEventListener('click', toonHamburger);
 hamburgerClose.addEventListener('click', toonHamburger)
 
-//wilde dit uitproberen dit is de quickview button
+//wilde dit uitproberen: dit is de quickview button
 var quickViewButton = document.querySelectorAll('#quickViewButton');
 var productArticle = document.querySelectorAll('#productArticle');
 
 productArticle.forEach(element => {
-    element.addEventListener('mouseover', (event) =>
-        event.target.classList.add('show')
-    )
+  element.addEventListener('mouseover', (event) =>
+    event.target.classList.add('show')
+  )
 
-    element.addEventListener('mouseout', (event) =>
-        event.target.classList.remove('show'))
+  element.addEventListener('mouseout', (event) =>
+    event.target.classList.remove('show'))
 })
 
-// hulp van Sam
+//hulp van Sam voor me automatische slider
 
 var slider = document.querySelectorAll(".article-slider article");
 console.log(slider)
@@ -36,35 +36,33 @@ var counter = 1;
 
 function moveArticle() {
 
-    console.log(counter)
-    slider.forEach(element => {
-        element.style.transform = "translateX(-" + counter * 2 + "00%)";
-    });
-    counter++
+  console.log(counter)
+  slider.forEach(element => {
+    element.style.transform = "translateX(-" + counter * 2 + "00%)";
+  });
+  counter++
 
-    if (counter == slider.length / 2) {
-        counter = 0;
-    }
+  if (counter == slider.length / 2) {
+    counter = 0;
+  }
 
-    console.log("fire")
+  console.log("fire")
 }
 
 window.setInterval(moveArticle, 8000);
 
 moveArticle()
 
-
-
-
+//uitklapbare footer
 var footerFirst = document.querySelector('footer section:nth-of-type(3) button')
 
 console.log(footerFirst)
 
 function toonFooter() {
-    document.body.classList.toggle('toonFooter');
-  }
+  document.body.classList.toggle('toonFooter');
+}
 
-  footerFirst.addEventListener('click', toonFooter)
+footerFirst.addEventListener('click', toonFooter)
 
 
 var footerSecond = document.querySelector('footer section:nth-of-type(4) button')
@@ -72,10 +70,10 @@ var footerSecond = document.querySelector('footer section:nth-of-type(4) button'
 console.log(footerSecond)
 
 function toonFooter2() {
-    document.body.classList.toggle('toonFooter2');
+  document.body.classList.toggle('toonFooter2');
 }
 
-  footerSecond.addEventListener('click', toonFooter2)
+footerSecond.addEventListener('click', toonFooter2)
 
 
 var footerThird = document.querySelector('footer section:nth-of-type(5) button')
@@ -83,7 +81,7 @@ var footerThird = document.querySelector('footer section:nth-of-type(5) button')
 console.log(footerThird)
 
 function toonFooter3() {
-    document.body.classList.toggle('toonFooter3');
-  }
+  document.body.classList.toggle('toonFooter3');
+}
 
-  footerThird.addEventListener('click', toonFooter3)
+footerThird.addEventListener('click', toonFooter3)
